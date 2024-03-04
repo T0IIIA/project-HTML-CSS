5c625c4f3811df8e4e02b7abebd71f57dd2c169e
AlexeyDvorianchuk <153747024+NataD81@users.noreply.github.com>
2024-03-04 19:30:06 +0200

    main.js button

--- src/main.js
+++ src/main.js
@@ -4,22 +4,30 @@ document.addEventListener('DOMContentLoaded', function() {
   var backdrop = document.querySelector('.backdrop');
   var mobileMenuNavLinks = document.querySelectorAll('.mobile-menu-nav a');
 
-  
   function openMobileMenu() {
     backdrop.classList.add('is-open');
   }
 
-  
   function closeMobileMenu() {
     backdrop.classList.remove('is-open');
   }
 
-  
   mobileMenuOpenBtn.addEventListener('click', openMobileMenu);
   mobileMenuCloseBtn.addEventListener('click', closeMobileMenu);
 
-  
   mobileMenuNavLinks.forEach(function(link) {
     link.addEventListener('click', closeMobileMenu);
+  });
+
+  
+  var shopNowLink = document.querySelector('.mobile-menu-button a');
+  shopNowLink.addEventListener('click', function(event) {
+    var targetSectionId = shopNowLink.getAttribute('href'); 
+    var targetSection = document.querySelector(targetSectionId); 
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: 'smooth' }); 
+      closeMobileMenu(); 
+      event.preventDefault(); 
+    }
   });
 });
